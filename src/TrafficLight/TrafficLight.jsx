05f27828a13b2ed0edd90762589f9f6fdadb39b8
whlong1 <whunterlong@gmail.com>
2021-10-24 16:31:30 -0400

    create traffic light component

---
+++ src/TrafficLight/TrafficLight.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+const TrafficLight = () => {
+
+
+
+   return (
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+         <div>R</div>
+         <div>Y</div>
+         <div>G</div>
+      </div>
+   )
+}
+
+export default TrafficLight
