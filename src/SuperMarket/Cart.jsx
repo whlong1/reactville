d2c11306d8b867398ad1da90bf852a68ddc4dce2
whlong1 <whunterlong@gmail.com>
2021-11-16 01:06:48 -0500

    adjust card background color and drop shadow

--- src/SuperMarket/Cart.jsx
+++ src/SuperMarket/Cart.jsx
@@ -12,7 +12,10 @@ const Cart = (props) => {
       {props.cart?.map((item, idx) => (
         <CartItem key={idx} item={item} removeFromCart={props.removeFromCart} />
       ))}
-      <p>Total: ${total.toFixed(2)}</p>
+      <div className="cart-total">
+        <p>Total:</p>
+        <p>${total.toFixed(2)}</p>
+      </div>
       <button>Checkout</button>
       <button onClick={() => props.setCart([])}>Clear Cart</button>
     </div>
