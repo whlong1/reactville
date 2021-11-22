95b1fb4ac82da438da613c6a28d99f5bfdbf5ae2
whlong1 <whunterlong@gmail.com>
2021-11-22 11:58:49 -0500

    create post office component

---
+++ src/PostOffice/PostOffice.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import './mail.css'
+
+
+
+const PostOffice = (props) => {
+	// create po box
+	// nested routes to display po boxes
+	// submit letter form
+	// add po box
+	// view letters from a specific po box
+	// select all po boxes from drop down menu
+	// select stamps from drop down
+	// charge user when sending letter
+
+
+	return (
+		<div className="post-office">
+			<nav>
+				<h1>Post Office</h1>
+			</nav>
+
+		</div>
+	)
+}
+
+export default PostOffice
