06cca2e14ef5729d1ad7b29f4900c4f3c73198b3
whlong1 <whunterlong@gmail.com>
2021-11-22 10:18:22 -0500

    add valid dollar amount error handling

--- src/Bank/Account.jsx
+++ src/Bank/Account.jsx
@@ -5,18 +5,12 @@ import ControlPanel from "./ControlPanel"
 const Account = (props) => {
 	const [balance, setBalance] = useState(0)
 	const [message, setMessage] = useState('')
-
 	const validDollarAmount = /^\d+(?:\.\d{0,2})?$/
   //https://regexlib.com/REDetails.aspx?regexp_id=543
 
-	const validateDollars = (value) => {
-		return validDollarAmount.test(value)
-	}
-
-
 	const handleBalance = (name, amount) => {
-		console.log(validateDollars(amount))
 		try {
+			if (!validDollarAmount.test(amount)) throw new Error('Please enter a valid number')
 			const value = name === 'Withdraw' ? amount *= -1 : amount
 			if (name === 'Withdraw' && balance + value < 0) throw new Error('Insufficient Funds')
 			if (!props.handleExchange(value)) throw new Error('Not enough cash!')
