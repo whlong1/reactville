c82588f7de0b8b4761721ae829a253a3f1ff1394
whlong1 <whunterlong@gmail.com>
2021-11-28 16:03:32 -0500

    update post office select menus

--- src/PostOffice/NewLetter/SelectBox.jsx
+++ src/PostOffice/NewLetter/SelectBox.jsx
@@ -3,9 +3,9 @@ import React from "react"
 const SelectBox = (props) => {
   return (
     <select required name="boxNo" onChange={(e) => props.setBoxNum(e.target.value)}>
-      <option value="">SELECT PO BOX</option>
+      <option value="">Select PO Box</option>
       {props.boxNumbers.map((num) => (
-        <option key={num} value={num}>PO BOX {num}</option>
+        <option key={num} value={num}>PO Box {num}</option>
       ))}
     </select>
   )
--- src/PostOffice/NewLetter/SelectRecipient.jsx
+++ src/PostOffice/NewLetter/SelectRecipient.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const SelectRecipient = (props) => {
   return (
     <select required name="recipient" onChange={props.handleChange}>
-      <option value="">SELECT RECIPIENT</option>
+      <option value="">Select Recipient</option>
       {props.selectedBox?.boxHolders.map((name, idx) => (
         <option key={idx} value={name}>{name}</option>
       ))}
