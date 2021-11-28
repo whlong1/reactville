6532ec0c8cc9d85a169e8aa677a80d3cca84c895
whlong1 <whunterlong@gmail.com>
2021-11-28 14:09:33 -0500

    apply new post office background img

--- src/PostOffice/BoxList.jsx
+++ src/PostOffice/BoxList.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import poImg from './assets/post-img.png'
 
 const BoxList = (props) => {
   const boxList = Object.keys(props.boxes)
 
   return (
     <div className="lobby">
-      <img className="image"></img>
+      <section className="img-container"><img src={poImg}/></section>
       <section className="po-box-list">
         {boxList.map((boxNo) => (
           <Link className="po-box" to={`/postoffice/${boxNo}`} key={boxNo}>
