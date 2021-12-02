c2a004688839c0e59b76f4d4a4164bf77b4c982c
whlong1 <whunterlong@gmail.com>
2021-12-02 17:17:49 -0500

    clean up landing component

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-
 import Day from '../../assets/Day.png'
 import Night from '../../assets/Night.png'
 
-const Landing = (props) => {
-
-  const background = props.daytime ? Day : Night
-
+const Landing = ({daytime}) => {
   return (
-    <div className="landing" style={{ backgroundImage: `url(${background})` }}>
+    <div className="landing" style={{ backgroundImage: `url(${daytime ? Day : Night})` }}>
       <h1 id="splash-logo">Reactville</h1>
     </div>
   )
