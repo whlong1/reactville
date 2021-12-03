d77d1086c12fa8090bcb615c3c2f4982de93c4ca
teradaian <teradaian@gmail.com>
2021-12-03 12:00:57 -0800

    ctidy up disabled list item

--- src/Components/BurgerShop/Ingredient.jsx
+++ src/Components/BurgerShop/Ingredient.jsx
@@ -4,10 +4,8 @@ const Ingredient = (props) => {
 	return (
 		props.disabled ? 
 		<li style={{ background: 'darkgray' }}>
-		{props.ingredient.name}
-		{!props.list &&
-			<button onClick={() => props.removeFromBurger(props.idx)}>X</button>
-		}
+			{props.ingredient.name}
+		{ !props.list && <button onClick={() => props.removeFromBurger(props.idx)}>X</button> }
 		</li>
 		:
 		<li style={{ background: props.ingredient.color }}>
