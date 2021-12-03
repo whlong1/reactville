e2b7ed527704b1a05669fc6b6d839fd3c8793d17
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2021-12-03 11:21:30 -0600

    finish po boxes

--- src/Components/PostOffice/BoxList.jsx
+++ src/Components/PostOffice/BoxList.jsx
@@ -6,18 +6,17 @@ const BoxList = (props) => {
 
   return (
     <section className="po-box-list">
-      <div className="box-banner">
-        <h1>Boxes</h1>
-      </div>
-      <div className="box-container">
-        {boxList.map((boxNo) => (
-          <Link className="po-box" to={`/postoffice/${boxNo}`} key={boxNo}>
-            <div className="inner-po-box">
-              BOX {boxNo}
-            </div>
-          </Link>
-        ))}
-      </div>
+      {boxList.map((boxNo) => (
+        <Link 
+          className="po-box masked-corners" 
+          to={`/postoffice/${boxNo}`} 
+          key={boxNo}
+        >
+          <div className="inner-po-box masked-corners">
+            BOX {boxNo}
+          </div>
+        </Link>
+      ))}
     </section>
   )
 }
