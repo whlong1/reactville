04d446d5c22422480172703167409aaa4bfe8d35
whlong1 <whunterlong@gmail.com>
2022-07-10 16:43:34 -0400

    clean up conditional rendering on display products

--- src/Components/SuperMarket/DisplayProducts.jsx
+++ src/Components/SuperMarket/DisplayProducts.jsx
@@ -2,19 +2,16 @@ import React from 'react'
 import Product from './Product'
 
 const DisplayProducts = (props) => {
-
-  const selectedProducts = props.products.map((product, idx) => {
-    return product.category === props.productCategory &&
-      <Product
-        key={idx}
-        product={product}
-        addToCart={props.addToCart}
-      />
-  })
-
   return (
     <div className="product-list">
-      {selectedProducts}
+      {props.products.map((product, idx) => (
+        product.category === props.productCategory &&
+        <Product
+          key={idx}
+          product={product}
+          addToCart={props.addToCart}
+        />
+      ))}
     </div>
   )
 }
