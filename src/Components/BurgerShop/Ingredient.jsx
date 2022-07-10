21263cf87325b3aaa4a74ba4524bef73e287f819
whlong1 <whunterlong@gmail.com>
2022-07-10 16:33:16 -0400

    clean up conditional inline styling on ingredient

--- src/Components/BurgerShop/Ingredient.jsx
+++ src/Components/BurgerShop/Ingredient.jsx
@@ -2,21 +2,15 @@ import React from "react"
 
 const Ingredient = (props) => {
 	return (
-		props.disabled ?
-			<li style={{ background: 'darkgray' }}>
-				{props.ingredient.name}
-				{!props.list && <button onClick={() => props.removeFromBurger(props.idx)}>X</button>}
-			</li>
-			:
-			<li style={{ background: props.ingredient.color }}>
-				{props.ingredient.name}
-				{props.addToBurger ?
-					<button onClick={() => props.addToBurger(props.ingredient)}>+</button>
-					:
-					<button onClick={() => props.removeFromBurger(props.idx)}>X</button>
-				}
-			</li>
+		<li style={props.disabled ? { background: 'darkgray' } : { background: props.ingredient.color }}>
+			{props.ingredient.name}
+			{props.addToBurger
+				? <button disabled={props.disabled} onClick={() => props.addToBurger(props.ingredient)}>+</button>
+				: <button onClick={() => props.removeFromBurger(props.idx)}>X</button>
+			}
+		</li>
 	)
 }
 
 export default Ingredient
+
