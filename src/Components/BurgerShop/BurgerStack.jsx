4b58200d4144427989a6996db2edca22e74a90b4
whlong1 <whunterlong@gmail.com>
2022-07-10 16:39:57 -0400

    clean up conditional rendering in burger stack

--- src/Components/BurgerShop/BurgerStack.jsx
+++ src/Components/BurgerShop/BurgerStack.jsx
@@ -2,22 +2,19 @@ import React from "react"
 import Ingredient from "./Ingredient"
 
 const BurgerStack = (props) => {
+
+  if (!props.ingredients.length) return <ul className="burger-stack">No Ingredients</ul>
+
   return (
     <ul className="burger-stack">
-      {props.ingredients.length ?
-        props.ingredients.map((ingredient, idx) => (
-          <Ingredient
-            key={idx}
-            idx={idx}
-            ingredient={ingredient}
-            removeFromBurger={props.removeFromBurger}
-          />
-        ))
-        :
-        <div>
-          No Ingredients
-        </div>
-      }
+      {props.ingredients.map((ingredient, idx) => (
+        <Ingredient
+          key={idx}
+          idx={idx}
+          ingredient={ingredient}
+          removeFromBurger={props.removeFromBurger}
+        />
+      ))}
     </ul>
   )
 }
