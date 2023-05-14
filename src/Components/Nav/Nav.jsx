f35671c3f3bc34b5a53f74badf2382f3f8644675
whlong1 <whunterlong@gmail.com>
2023-05-14 15:27:05 -0400

    Build out basic WeatherStatus component

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,25 +1,12 @@
-import { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 
 // Components
 import Clock from './Clock'
 import Wallet from "./Wallet"
+import WeatherStatus from './WeatherStatus'
 import Logo from '../../assets/react-logo.png'
 
-// Services
-import { getWeatherDataFromAPI } from '../../services/weatherService'
-
 const Nav = (props) => {
-  const getWeatherInfo = async () => {
-    navigator.geolocation.getCurrentPosition(async ({ coords: { latitude, longitude } }) => {
-      const data = await getWeatherDataFromAPI(latitude, longitude)
-      console.log(data)
-    })
-  }
-
-  useEffect(() => {
-    getWeatherInfo()
-  }, [])
 
   return (
     <nav className="navigation-bar">
@@ -30,6 +17,7 @@ const Nav = (props) => {
       <NavLink to='/postoffice'>POST OFFICE</NavLink>
       <Wallet cash={props.cash} />
       <Clock daytime={props.daytime} setDaytime={props.setDaytime} />
+      <WeatherStatus />
     </nav>
   )
 }
--- src/Components/Nav/WeatherStatus.jsx
+++ src/Components/Nav/WeatherStatus.jsx
@@ -1,8 +1,23 @@
-
+import { useState, useEffect } from 'react'
+import { getWeatherDataFromAPI } from '../../services/weatherService'
 
 const WeatherStatus = () => {
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    const getWeatherInfo = async () => {
+      navigator.geolocation.getCurrentPosition(async ({ coords: { latitude, longitude } }) => {
+        const data = await getWeatherDataFromAPI(latitude, longitude)
+        setWeather(data)
+      })
+    }
+    getWeatherInfo()
+  }, [])
+
+  if (!weather) return <img className="display-items" src="" alt="" />
+
   return (
-    <img src="" alt="" />
+    <p className="display-items">{weather.current_weather.weathercode}</p>
   )
 }
 
